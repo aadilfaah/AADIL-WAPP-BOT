a2bf16456c772a12729f35ad85355aaa1f256db3
aadilfaah <aadil.faah@gmail.com>
2026-04-09 23:54:33 +0600

    Update and rename main.py to render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x google-chrome-stable_current_amd64.deb .
+fi
+
+export PATH=$PATH:$STORAGE_DIR/chrome/opt/google/chrome
